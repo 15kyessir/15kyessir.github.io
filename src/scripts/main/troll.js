e25f85be2c96a6bc372777d3b00545b2927cef66
Reddstone35 <g.reddstone@gmail.com>
2022-03-26 20:03:44 -0600

    u

---
+++ src/scripts/main/troll.js
@@ -0,0 +1,8 @@
+var number = Math.floor((Math.random() * 4) + 1)
+if (number == 2) {
+  if (window.location.pathname === "/socials/index.html" || window.location.pathname === "/socials/" ) {
+    window.location.href = "https://reddstone35.com/socials/"
+  } else {
+    window.location.href = "https://reddstone35.com/"
+  }
+}
