1f7f4e1d917008b594b19f065e8c91f89b1f8764
Reddstone35 <g.reddstone@gmail.com>
2022-03-27 17:09:04 -0600

    hi

--- src/scripts/main/troll.js
+++ src/scripts/main/troll.js
@@ -1,8 +1,8 @@
-var number = Math.floor((Math.random() * 7) + 1)
-if (number == 4) {
-  if (window.location.pathname === "/socials/index.html" || window.location.pathname === "/socials/" ) {
-    window.location.href = "https://reddstone35.com/socials/"
-  } else {
-    window.location.href = "https://reddstone35.com/"
-  }
-}
+// var number = Math.floor((Math.random() * 100000000) + 1)
+// if (number == -1) {
+//   if (window.location.pathname === "/socials/index.html" || window.location.pathname === "/socials/" ) {
+//     window.location.href = "https://reddstone35.com/socials/"
+//   } else {
+//     window.location.href = "https://reddstone35.com/"
+//   }
+// }
